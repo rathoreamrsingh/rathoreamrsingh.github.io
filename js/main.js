634f84df54bdc12bbf4b3e1395a33afac0985f0d
Amar Singh <amarsingh@outlook.in>
2019-08-11 16:14:40 +0530

    amar|added few more changes in skills and other section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+const menuSelect = (event, menu) => {
+    removeAllSelectedMenu(event);
+    $("#" + menu + "-menu").addClass("active");
+};
 
+const removeAllSelectedMenu = (event) => {
+    $("#home-menu").removeClass("active");
+    $("#about-menu").removeClass("active");
+    $("#skills-menu").removeClass("active");
+    $("#projects-menu").removeClass("active");
+    $("#experience-menu").removeClass("active");
+    $("#achievements-menu").removeClass("active");
+};
